158a42a87dbca7852e67fbc9eadb04d31d6eb419
Jussi Nokelainen <jussinoke@gmail.com>
2026-07-29 21:37:47 +0300

    added personal best into shown stats

--- src/tuipe/render.rs
+++ src/tuipe/render.rs
@@ -239,10 +239,19 @@ impl Tuipe {
                 let mut total_wpm = 0.0;
                 let mut total_wpm_raw = 0.0;
                 let mut total_acc = 0.0;
+                let mut record_wpm = 0.0;
+                let mut record_language = String::new();
+                let mut record_ttype = String::new();
                 for result in &results {
                     total_wpm += result.wpm;
                     total_wpm_raw += result.raw_wpm;
                     total_acc += result.accuracy;
+
+                    if result.wpm > record_wpm {
+                        record_wpm = result.wpm;
+                        record_language = result.language.clone();
+                        record_ttype = result.test_type.clone();
+                    }
                 }
                 let avg_wpm = ((total_wpm / tests_count as f64) * 10.0).round() / 10.0;
                 let avg_wpm_raw = ((total_wpm_raw / tests_count as f64) * 10.0).round() / 10.0;
@@ -253,6 +262,28 @@ impl Tuipe {
                     Style::default().fg(Color::Cyan),
                 )));
                 lines.push(Line::from(Span::raw("")));
+                lines.push(Line::from(Span::styled(
+                    "Personal best:",
+                    Style::default().fg(Color::Green),
+                )));
+                lines.push(Line::from(Span::raw(format!(
+                    "Test type: {}",
+                    record_ttype
+                ))));
+                lines.push(Line::from(Span::raw(format!(
+                    "wpm: {}",
+                    (record_wpm * 10.0).round() / 10.0
+                ))));
+                lines.push(Line::from(Span::raw(format!(
+                    "Language: {}",
+                    record_language
+                ))));
+
+                lines.push(Line::from(Span::raw("")));
+                lines.push(Line::from(Span::styled(
+                    "Overall:",
+                    Style::default().fg(Color::Green),
+                )));
                 lines.push(Line::from(Span::raw(format!(
                     "Tests completed: {}",
                     tests_count
